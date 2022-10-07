2726541739890f5b1dfd27c5d33a99e16af49ff5
LeandroFerrero <leaferrero@gmail.com>
2022-10-07 11:22:28 -0300

    Menu y galeria

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// CIERRA MENU AL DARLE CLICK A ALGUNA OPCION 
+
+const menu = document.querySelector(".nav__menu");
+const navegador = document.querySelector(".nav__input");
+
+
+if (menu) {
+  menu.addEventListener('click', () => {
+    console.log('menu clicked');
+    navegador.checked = false;
+  });
+}
+
+
+
+
+
