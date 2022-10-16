bc4d2ff171f9cc09a2329762c332fe36e477dff7
LeandroFerrero <leaferrero@gmail.com>
2022-10-16 16:49:10 -0300

    Version Previa sin verificar formulario

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 
 // CIERRA MENU AL DARLE CLICK A ALGUNA OPCION 
 
-const menu = document.querySelector(".nav__menu");
-const navegador = document.querySelector(".nav__input");
+// const menu = document.querySelector(".nav__menu");
+// const navegador = document.querySelector(".nav__input");
 
-if (menu) {
-  menu.addEventListener('click', () => {
-    console.log('menu clicked');
-    navegador.checked = false;
-  });
+// if (menu) {
+//   menu.addEventListener('click', () => {
+//     navegador.checked = false;
+//   });
+// }
+
+const links = document.querySelectorAll('.nav__item');
+
+for (var i = 0; i < links.length; i++) {
+  links[i].onclick = cerrarMenu;
 }
 
-
+function cerrarMenu(){
+  document.querySelector('.nav__input').checked = false;
+}
 
 
 
