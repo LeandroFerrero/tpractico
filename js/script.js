9a34905a39ee2ba0a9d76751e45715f7b7e07d5c
LeandroFerrero <leaferrero@gmail.com>
2022-10-21 12:08:34 -0300

    version sin api-rest

--- js/script.js
+++ js/script.js
@@ -13,37 +13,53 @@ function cerrarMenu() {
 /*---------------   Validacion Formulario --------------------*/
 
 document.addEventListener("DOMContentLoaded", function () {
-  var formulario = document.querySelector(".form").addEventListener('submit', validarFormulario);
+document.querySelector(".form").addEventListener('submit', validarFormulario);
 });
+
+function despliegaAlerta(msgError){
+  document.querySelector('#btn__alerta').checked = true;
+  document.querySelector('#mensaje').innerHTML = msgError;
+};
+
 
 function validarFormulario(evento) {
   evento.preventDefault();
   const nombre = document.querySelector('#form_nombre').value;
 
   if (nombre.length == 0) {
-
-    alert("El nombre esta vacio. debes ingresar un nombre")
-
+    msgError = "El Nombre no puede estar vacio.";
+    despliegaAlerta(msgError);
     return;
   }
 
+  
   const mail = document.querySelector('#form_mail').value;
 
   if (mail.length == 0) {
-
-    alert("El mail esta vacio. Debes ingresar un mail")
-
+    msgError = "El Email no puede estar vacio.";
+    despliegaAlerta(msgError);
     return;
-  }
+  } 
+
+  if (!((/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i).test(mail))){
+    msgError = "Formato de Email incorrecto.";
+    despliegaAlerta(msgError);
+    return;
+  } 
+
 
   const mensaje = document.querySelector('#form_mensaje').value;
 
   if (mensaje.length == 0) {
-
-    alert("El mensaje esta vacio, debes ingresar un mensaje.")
-
+    msgError = "El Mensaje no puede estar vacio.";
+    despliegaAlerta(msgError);
     return;
   }
+
+
   this.submit();
+  
+  document.querySelector(".form").reset();
+
 }
 
