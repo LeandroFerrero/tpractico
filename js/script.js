79ad857a7f1475d3cac90d6472c8e6228ed720de
LeandroFerrero <leaferrero@gmail.com>
2022-10-23 19:49:28 -0300

    Version con Api

--- js/script.js
+++ js/script.js
@@ -10,17 +10,66 @@ function cerrarMenu() {
   document.querySelector('.nav__input').checked = false;
 }
 
-/*---------------   Validacion Formulario --------------------*/
+/*------------------------------------------------------------------------------------------------*/
+/*---------------   Validacion Formulario , mensaje error ----------------------------------------*/
+/*------------------  y publicacion comentarios desde api ----------------------------------------*/
+/*------------------------------------------------------------------------------------------------*/
 
-document.addEventListener("DOMContentLoaded", function () {
-document.querySelector(".form").addEventListener('submit', validarFormulario);
-});
+
+
+
+
+/*------------------------------------funcion para llamar a la api----------------------*/
+function publicarComentariosApi() {
+  
+  contenido=document.querySelector('#coment');
+
+  var comentarios =['Feliicitaciones por la pagina, esta buenisima, me gustaria que me pasen los datos de los desarrolladores para contratarlos, gracias','Que alegria saber que el 30 estaran en Cordoba. Los seguimos a todos lados asi que alli estaremos. Los queremos mucho!!!!!', 'Por favor pongan mas fechas, las entradas se agotan enseguida y nos quedamos afuera!!!! ' ];
+
+  fetch('https://randomuser.me/api/?results=3&nat=es') 
+
+
+
+  .then(res => res.json())
+  .then(data => {
+  
+    let salida = "";
+    for (var i = 0; i < 3; i++){
+    
+      salida += `<div class="tarjeta">
+                  <div class="tarjeta__foto">
+                    <img src="${data.results[i].picture.large}" class="circulo__comentario" alt="foto_usuario"></img> 
+                  </div>
+                  <div class="tarjeta__nombre">
+                    <h3>${data.results[i].name.first}</h3>
+                  </div>
+                  <div class="tarjeta__comentario">
+                    <p>${comentarios[i]}</p>
+                  </div>
+                </div>`;
+
+    }
+    contenido.innerHTML= salida;
+  })
+     
+      
+}
+
+
+/*-----------------------funcion despliega  mensaje de error-----------------*/
 
 function despliegaAlerta(msgError){
   document.querySelector('#btn__alerta').checked = true;
   document.querySelector('#mensaje').innerHTML = msgError;
 };
 
+
+
+/*---------------- funcion para validar mail ------------------------*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".form").addEventListener('submit', validarFormulario);
+  });
 
 function validarFormulario(evento) {
   evento.preventDefault();
@@ -56,10 +105,21 @@ function validarFormulario(evento) {
     return;
   }
 
-
-  this.submit();
   
   document.querySelector(".form").reset();
-
+  publicarComentariosApi();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
